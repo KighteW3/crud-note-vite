d19f0eb13f66d3fc4264f639be7a48eb3bd0bf1e
Juan david <117365032+juanprogram1@users.noreply.github.com>
2024-05-30 19:17:08 -0500

    Update bottomCreate, textContent
    se agrego transcision al boton de mostrar y tambien se arreglo lo de los recuadros de la informacion del form

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import { handleForm, getNowDate } from "./btnSave";
-
 // bottomShow Form
+document.getElementById("bottomShow")?.addEventListener("click", function () {
+  const bottomShow = document.getElementById("bottomShow") as HTMLButtonElement;
+  const hiddenContent = document.getElementById("hiddenContent") as HTMLElement;
+
+  if (bottomShow && hiddenContent) {
+    //  Hidden the button and show the hidden content
+    bottomShow.style.display = "none";
+
+    hiddenContent.classList.remove("hidden");
+    hiddenContent.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 1200,
+      easing: "ease-in-out",
+    });
+  }
+});
+
+// bottomShow Form style header and footer
 document.getElementById("bottomShow")?.addEventListener("click", function () {
   const bottomShow = document.getElementById("bottomShow");
   const bottomHeader = document.getElementById("bottomHeader");
   const bottomFooter = document.getElementById("bottomFooter");
   const hiddenContent = document.getElementById("hiddenContent");
   if (bottomShow && bottomHeader && bottomFooter && hiddenContent) {
-    //  Hidden the button and show the hidden content
-    bottomShow.style.display = "none";
     // header source specifications
     bottomHeader.style.textTransform = "uppercase";
     bottomHeader.style.fontFamily = "Roboto";
@@ -24,18 +38,6 @@ document.getElementById("bottomShow")?.addEventListener("click", function () {
     bottomFooter.style.letterSpacing = "1px";
     bottomFooter.style.paddingBottom = "8px";
     bottomFooter.style.textTransform = "uppercase";
-
-    // (hidden content)
-    hiddenContent.classList.remove("hidden");
-  } else {
-    // Show the button and hide the hidden content
-    if (bottomShow && hiddenContent !== null) {
-      // Ensures that bottomShow is not null before attempting to change its display property
-      bottomShow.style.display = "block";
-
-      // Ensures that hiddenContent is not null before attempting to add the class "hidden"
-      hiddenContent.classList.add("hidden");
-    }
   }
 });
 
